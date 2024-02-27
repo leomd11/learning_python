17d8aa45675ce48e0f52f81a206fdaf1433fa0d8
leomd11 <leonardodelfim13@gmail.com>
2024-02-26 21:46:42 -0300

    Calculadora Simples

--- Calculadora.py
+++ Calculadora.py
@@ -1,3 +1,23 @@
-# Calculadora
+# Calculadora Simples
 
-print("segundo teste")
+print("Selecione o número da operação desejada: \n\n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n")
+
+opert = int(input("Digite sua opção (1/2/3/4): "))
+
+num1 = float(input("Digite o primeiro número: "))
+
+num2 = float(input("Digite o segundo número: "))
+print("\n")
+
+if opert == 1:
+    soma = num1 + num2
+    print("%s + %r =" %(num1,num2), soma) 
+elif opert == 2:
+    sub = num1 - num2
+    print("%s - %r =" %(num1,num2), sub) 
+elif opert == 3:
+    multi = num1 * num2
+    print("%s x %r =" %(num1,num2), multi) 
+else:
+    div = round(num1 / num2, 2)
+    print("%s / %r =" %(num1,num2), div)
